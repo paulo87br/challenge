c9f2c67a98badde05c0c9f74a585c80b57e6eeff
Paulo Nascimento <paulo.joy@gmail.com>
2026-09-16 11:09:15 -0300

    feat: add application layout

---
+++ app/layout.tsx
@@ -0,0 +1,3 @@
+import './globals.css';
+export const metadata={title:'Aranis Challenge',description:'Dynamic domain simulation'};
+export default function Layout({children}:{children:React.ReactNode}){return <html lang="pt-BR"><body>{children}</body></html>}
